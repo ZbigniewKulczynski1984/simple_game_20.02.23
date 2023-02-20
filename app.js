5302db24ebbc394c10f065e9ea0f47959efe4b54
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-02-20 14:30:00 +0100

    code added

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
-const hero = document.getElementById('hero')
-const block = document.getElementById('block')
-const btn = document.querySelector('.btn')
+const hero = document.getElementById('hero');
+const block = document.getElementById('block');
+const btn = document.querySelector('.btn');
 
 const jump = () => {
-    if(hero.classList != 'animate') {
-        hero.classList.add('animate')
+	if (hero.classList != 'animate') {
+		hero.classList.add('animate');
+	}
+	setTimeout(function () {
+		hero.classList.remove('animate');
+	}, 500);
+};
+
+const youAreDead = setInterval(function () {
+	const heroTop = parseInt(window.getComputedStyle(hero).getPropertyValue('top'));
+
+    const blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'));
+
+    if(blockLeft < 20 && blockLeft > 0 && heroTop >= 130) {
+        block.style.animation = 'none'
+        block.style.display = 'none'
+        alert('you lose');
     }
-    setTimeout(function(){
-        hero.classList.remove('animate')
-    },500)
-}
 
-btn.addEventListener('click', jump)
+}, 10);
 
+btn.addEventListener('click', jump);
