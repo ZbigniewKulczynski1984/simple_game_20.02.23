9756ade7a5d81f4bffb7ff7909e20e13359fc056
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-02-20 14:18:40 +0100

    info, code and style added

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 const hero = document.getElementById('hero')
 const block = document.getElementById('block')
+const btn = document.querySelector('.btn')
+
+const jump = () => {
+    if(hero.classList != 'animate') {
+        hero.classList.add('animate')
+    }
+    setTimeout(function(){
+        hero.classList.remove('animate')
+    },500)
+}
+
+btn.addEventListener('click', jump)
+
